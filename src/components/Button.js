f14614663e242d59d36b270c01e053c1b373c524
ahmedMoussa2020 <ahmed.moussaisman92@gmail.com>
2022-01-27 11:43:17 +0400

    dayList

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 import "components/Button.scss";
+import classNames from 'classnames';
 
 export default function Button(props) {
-   // return <></>;
+   const { confirm, danger } = props;
+   const buttonClass = classNames(
+      "button",
+      { "button--confirm": confirm },
+      { "button--danger": danger }
+   );
+
+   return (
+      <button
+         className={buttonClass}
+         disabled={props.disabled}
+         onClick={props.onClick}
+      >
+         {props.children}
+      </button>
+   );
 }
