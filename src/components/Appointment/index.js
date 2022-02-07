ccda0cf1e7bb829db78682c143c2d2923ce40648
ahmedMoussa2020 <ahmed.moussaisman92@gmail.com>
2022-02-07 23:32:57 +0400

    small changes

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -19,8 +19,6 @@ const EDIT = 'EDIT';
 const ERROR_SAVE = 'ERROR_SAVE';
 const ERROR_DELETE = 'ERROR_DELETE';
 
-
-
 export default function Appointment(props) {
   const { mode, back, transition } = useVisualMode(
     props.interview ? SHOW : EMPTY
